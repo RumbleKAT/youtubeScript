349158ac9cec2b0c2653fea1c0a98418d847b82a
Song Myung Jin <reki318@naver.com>
2016-09-25 02:09:34 +0900

    no message

---
+++ mp3converter/mp3converter/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace mp3converter
+{
+    partial class StudyEV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btndownload = new System.Windows.Forms.Button();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.cboresolution = new System.Windows.Forms.ComboBox();
+            this.Resolustion = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblPercentage = new System.Windows.Forms.Label();
+            this.makemp3 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(408, 28);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(142, 41);
+            this.button8.TabIndex = 0;
+            this.button8.Text = "convert WAV";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "url:";
+            // 
+            // btndownload
+            // 
+            this.btndownload.Location = new System.Drawing.Point(408, 247);
+            this.btndownload.Name = "btndownload";
+            this.btndownload.Size = new System.Drawing.Size(108, 30);
+            this.btndownload.TabIndex = 2;
+            this.btndownload.Text = "Download";
+            this.btndownload.UseVisualStyleBackColor = true;
+            this.btndownload.Click += new System.EventHandler(this.btndownload_Click);
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(51, 107);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(337, 28);
+            this.txturl.TabIndex = 3;
+            // 
+            // cboresolution
+            // 
+            this.cboresolution.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboresolution.FormattingEnabled = true;
+            this.cboresolution.Items.AddRange(new object[] {
+            "360",
+            "480\t",
+            "720"});
+            this.cboresolution.Location = new System.Drawing.Point(120, 150);
+            this.cboresolution.Name = "cboresolution";
+            this.cboresolution.Size = new System.Drawing.Size(268, 26);
+            this.cboresolution.TabIndex = 4;
+            // 
+            // Resolustion
+            // 
+            this.Resolustion.AutoSize = true;
+            this.Resolustion.Location = new System.Drawing.Point(12, 154);
+            this.Resolustion.Name = "Resolustion";
+            this.Resolustion.Size = new System.Drawing.Size(108, 18);
+            this.Resolustion.TabIndex = 5;
+            this.Resolustion.Text = "Resolustion:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 198);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(501, 38);
+            this.progressBar.TabIndex = 6;
+            // 
+            // lblPercentage
+            // 
+            this.lblPercentage.AutoSize = true;
+            this.lblPercentage.Location = new System.Drawing.Point(522, 207);
+            this.lblPercentage.Name = "lblPercentage";
+            this.lblPercentage.Size = new System.Drawing.Size(32, 18);
+            this.lblPercentage.TabIndex = 7;
+            this.lblPercentage.Text = "0%";
+            // 
+            // makemp3
+            // 
+            this.makemp3.Location = new System.Drawing.Point(211, 28);
+            this.makemp3.Name = "makemp3";
+            this.makemp3.Size = new System.Drawing.Size(158, 41);
+            this.makemp3.TabIndex = 8;
+            this.makemp3.Text = "MakeMp3";
+            this.makemp3.UseVisualStyleBackColor = true;
+            this.makemp3.Click += new System.EventHandler(this.makemp3_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(25, 28);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(158, 41);
+            this.button9.TabIndex = 9;
+            this.button9.Text = "mp4toFlv";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // StudyEV
+            // 
+            this.ClientSize = new System.Drawing.Size(585, 314);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.makemp3);
+            this.Controls.Add(this.lblPercentage);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.Resolustion);
+            this.Controls.Add(this.cboresolution);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.btndownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Name = "StudyEV";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button save1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.OpenFileDialog open;
+        private System.Windows.Forms.SaveFileDialog save;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btndownload;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.ComboBox cboresolution;
+        private System.Windows.Forms.Label Resolustion;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblPercentage;
+        private System.Windows.Forms.Button makemp3;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ mp3converter/mp3converter/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using YoutubeExtractor;
+using System.IO;
+using System.Threading;
+using JDP;
+using MediaToolkit.Model;
+using MediaToolkit.Options;
+using MediaToolkit;
+using WMPLib;
+
+
+namespace mp3converter
+{
+    public partial class StudyEV : Form
+    {
+        public StudyEV()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cboresolution.SelectedIndex = 0;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "MP3 File (*.mp3)|*.mp3;";
+            if (open.ShowDialog() != DialogResult.OK) return;
+
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "WAV File (*.wav)|*.wav;";
+            if (save.ShowDialog() != DialogResult.OK) return;
+
+            using (Mp3FileReader mp3 = new Mp3FileReader(open.FileName))
+            {
+                using (WaveStream pcm = WaveFormatConversionStream.CreatePcmStream(mp3))
+                {
+                    WaveFileWriter.CreateWaveFile(save.FileName, pcm);
+                }
+            }
+        }
+
+        private void btndownload_Click(object sender, EventArgs e)
+        {
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            IEnumerable<VideoInfo> videos = DownloadUrlResolver.GetDownloadUrls(txturl.Text);
+            VideoInfo video = videos.First(p => p.VideoType == VideoType.Mp4 && p.Resolution == Convert.ToInt32(cboresolution.Text));
+            if (video.RequiresDecryption)
+                DownloadUrlResolver.DecryptDownloadUrl(video);
+            VideoDownloader downloader = new VideoDownloader(video, Path.Combine(Application.StartupPath + "\\", video.Title + video.VideoExtension));
+            downloader.DownloadProgressChanged += Downloader_DownloadProgressChanged;
+            Thread thread = new Thread(() => { downloader.Execute(); }) { IsBackground = true };
+            thread.Start();
+        }
+
+        private void Downloader_DownloadProgressChanged(object sender, ProgressEventArgs e)
+        {
+            Invoke(new MethodInvoker(delegate ()
+            {
+                progressBar.Value = (int)e.ProgressPercentage;
+                lblPercentage.Text = $"{string.Format("{0:0.##}", e.ProgressPercentage)}%";
+                progressBar.Update();
+            }));
+        }
+
+        private void makemp3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "FLV File (*.flv)|*.flv;";
+            if (open.ShowDialog() != DialogResult.OK) return;
+
+            FLVFile test = new FLVFile(open.FileName);
+            test.ExtractStreams(true, false, false, null);
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "MP4 File (*.mp4)|*.mp4;";
+            if (open.ShowDialog() != DialogResult.OK) return;
+
+            var inputFile = new MediaFile { Filename = open.FileName };
+            var outputFile = new MediaFile { Filename = @"C:\Users\송명진\Desktop\save.flv" };
+            
+             var player = new WindowsMediaPlayer();
+             var clip = player.newMedia(open.FileName);
+             //Console.WriteLine(TimeSpan.FromSeconds(clip.duration));
+
+             var conversionOptions = new ConversionOptions
+             {
+                 MaxVideoDuration = TimeSpan.FromSeconds(clip.duration), //몇초까지 설정할 것인지
+                 VideoAspectRatio = VideoAspectRatio.R16_9,
+                 VideoSize = VideoSize.Hd1080,
+                 AudioSampleRate = AudioSampleRate.Hz44100
+             };
+
+             using (var engine = new Engine())
+             {
+                 engine.Convert(inputFile, outputFile, conversionOptions);
+            }
+                MessageBox.Show("파일 전환이 완료되었습니다.");
+        }
+
+    }
+}
